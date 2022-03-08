db1f779ee8e44c1c6b29904cadc153ee3a7a924f
MykeMyke <mykemyke7356@gmail.com>
2022-03-08 01:43:05 +0000

    Links Updated

--- webcalendar/src/content.jsx
+++ webcalendar/src/content.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
-import { Paper, Grid } from "@mui/material";
+import { Paper, Grid, Typography } from "@mui/material";
 import { getGames } from "./api/games";
 import Game from "./calendarCard";
+import Container from "@mui/material/Container";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -26,13 +27,26 @@ export default function Content() {
   }, []);
   //Only filtering for future games at present
   const filteredData = data.filter((x) => Date.parse(x.datetime) > new Date());
+
   return (
-    <Grid container spacing={3} justify="center" sx={{ pl: 2 }}>
-      {filteredData.map((gameData) => (
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Game {...gameData} />
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Container sx={{ mb: 3 }}>
+        <Typography variant="h5">
+          There are {filteredData.length} games scheduled in the near future
+        </Typography>
+        <Typography variant="subtitle1">
+          Signups take place on{" "}
+          <a href="https://discord.gg/tridengames">the Triden Discord server</a>
+          .
+        </Typography>
+      </Container>
+      <Grid container spacing={3} justify="center" sx={{ pl: 2 }}>
+        {filteredData.map((gameData) => (
+          <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Game {...gameData} />
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
