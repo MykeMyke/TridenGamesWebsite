bed58bf1575e9b1ee7b45c066fc5b10c976e447f
MykeMyke <mykemyke7356@gmail.com>
2022-03-28 12:53:31 +0100

    gCalendar link implemented with Localisation issue

--- src/components/CalendarAddPopover.jsx
+++ src/components/CalendarAddPopover.jsx
@@ -2,9 +2,6 @@ import * as React from "react";
 import Popover from "@mui/material/Popover";
 import Button from "@mui/material/Button";
 import CalendarLink from "./CalendarLink";
-import Grid from "@mui/material/Grid";
-import Divider from "@mui/material/Divider";
-import Typography from "@mui/material/Typography";
 
 export default function CalendarAddPopover(props) {
   const { game } = props;
@@ -46,27 +43,7 @@ export default function CalendarAddPopover(props) {
         }}
         BackdropProps={{ invisible: false }}
       >
-        <Grid container direction="column" sx={{ px: 1, py: 1 }}>
-          <Typography variant="cardmain">Add game to Calendars:</Typography>
-          <Divider
-            variant="middle"
-            sx={{
-              mt: 1.2,
-              mb: 0.6,
-            }}
-          />
-          <CalendarLink game={game} />
-          <Divider
-            variant="middle"
-            sx={{
-              mt: 1.2,
-              mb: 0.6,
-            }}
-          />
-          <a href="" target="_blank" rel="noreferrer">
-            Google (coming soon)
-          </a>{" "}
-        </Grid>
+        <CalendarLink game={game} />
       </Popover>
     </div>
   );
