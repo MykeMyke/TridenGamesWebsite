dd855c1a28e1ad2afa2fd60d92af81181ce88d7e
MykeMyke <mykemyke7356@gmail.com>
2022-10-04 02:30:51 +0100

    Policy Refinement

--- src/data/policies.js
+++ src/data/policies.js
@@ -2,7 +2,6 @@ export const policies = [
   {
     name: "Server Rules",
     updated: "08-Mar-2022",
-    preamble: "",
     points: [
       "Be welcoming & respectful. No bullying, discrimination, or harassment of any kind (e.g. berating, targeting, harassment), whether directed to individuals or groups. No offensive, hurtful, discriminatory, or profane language in the chats or in nicknames. No references to real-world politics or religion.",
       "Be positive & supportive. Keep the conversation on topic and don't criticize people. Avoid accusatory language, or being overtly argumentative. Deliberate baiting or trolling will not be tolerated.",
@@ -18,7 +17,6 @@ export const policies = [
   {
     name: "Disciplinary Policy",
     updated: "08-Mar-2022",
-    preamble: "",
     points: [
       "Any contravention of the Server Rules will be investigated by a Moderator.  The Moderators are empowered to issue a warning / penalty at their discretion, up to and including a ban from the server.",
       "If a DM asks for any information prior to a session this must be submitted 24 hours in advance, or longer if specified by the DM.  Players must also give a minimum of 2 hours notice to the DM in the event that they are unable to attend, to enable another member to enjoy the experience. If this notice is not possible, they should message the DM as soon as possible with a brief explanation.  Real life can of course get in the way, but there is an expectation for all members to treat their DMs with respect wherever possible. DMs may report no-shows / no-contact to a Moderator.  Resident DMs will report all such instances 24 hours after game time.  The server operates a 3 strike policy for unexplained no-shows.  Strikes last for a year, are recorded, and are visible to server Staff and Resident DMs.  A third strike will result in a ban from signing up to Resident DM games.",
@@ -29,7 +27,6 @@ export const policies = [
   {
     name: "Streaming Policy",
     updated: "23-Jan-2022",
-    preamble: "",
     points: [
       "If DMs want to stream a game, this must be advertised on the listing along with the platform / service it will be streamed to.",
       "All players must explicitly consent to being streamed.",
@@ -42,7 +39,6 @@ export const policies = [
   {
     name: "Confidentiality Policy",
     updated: "08-Mar-2022",
-    preamble: "",
     points: [
       "During the running of this server, information will only be disclosed to those people who need to know it, like decision-making staff. Even then, the amount of that information is limited to the amount that is necessary for decision-making.",
     ],
@@ -51,7 +47,6 @@ export const policies = [
   {
     name: "Duplicate Signup Policy",
     updated: "29-Jun-2022",
-    preamble: "",
     points: [
       "You may sign up to the same module more than once in the same month. People may want to do this for many reasons, for example if perhaps their availability isn’t known. If signed up to duplicates of a module and  you play in one of them, please drop from the alternates to allow others a chance to experience it.",
     ],
@@ -60,7 +55,6 @@ export const policies = [
   {
     name: "Discord Use Policy",
     updated: "10-Aug-2022",
-    preamble: "",
     points: [
       "Check the pins of any given discord channel they use and follow any specific instructions given.",
       "Use threads to contain open ended / wide ranging conversations. You can press the plus or hash button when creating a message to turn it into a thread. For more information on threads please see: https://support.discord.com/hc/en-us/articles/4403205878423-Threads-FAQ",
@@ -70,7 +64,6 @@ export const policies = [
   {
     name: "Game Cancellation Policy",
     updated: "15-Sep-2022",
-    preamble: "",
     points: [
       "We make every effort to run games as scheduled, but occasionally real life gets in the way… even for DMs! Before cancelling a game a Resident DM will check whether another can cover it, if this is not possible the game will be cancelled and as a gesture of good will a bonus sign up credit will be given to each listed player.",
       "In some cases, a Resident DM will re-arrange the game, or run it in the next month's schedule. However, we cannot guarantee this will happen in all cases.",
