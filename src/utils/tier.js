765e159a581869e9cd4a1007e90b9a75f06eaf19
MykeMyke <72384771+MykeMyke@users.noreply.github.com>
2023-01-29 12:35:25 +0000

    Revert "Issue #4: Add Tier images for cards"

--- src/utils/tier.js
+++ src/utils/tier.js
@@ -11,21 +11,6 @@ export function checkTier(min, max) {
   } else {
     Tier = "(Lvl " + min + " to " + max + ")";
   }
-  return Tier;
-}
-
-export function checkTierImage(min, max) {
-  let Tier = "";
-  if (min === 1 && max === 4) {
-    Tier = "tier1";
-  } else if (min === 5 && max === 10) {
-    Tier = "tier2";
-  } else if (min === 11 && max === 16) {
-    Tier = "tier3";
-  } else if (min === 17 && max === 20) {
-    Tier = "tier4";
-  } else {
-    Tier = "specialtier";
-  }
+  console.log(Tier);
   return Tier;
 }
