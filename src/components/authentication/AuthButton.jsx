3eb30b8c3fa0e3086aba54b406995a513b8137eb
OperationXen <OperationXen@apocryphasecurity.com>
2023-03-24 21:13:17 +0000

    Added first pass of auth against discord

---
+++ src/components/authentication/AuthButton.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import { Avatar, Menu, MenuItem } from "@mui/material";
+
+export default function AuthButton() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const openMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const closeMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const discordAuth = () => {
+    const url = "/discord_auth/login/";
+
+    window.open(url, "_blank", "noreferrer");
+    closeMenu();
+  };
+
+  return (
+    <React.Fragment>
+      <Avatar onClick={openMenu} />
+      <Menu
+        id="auth-menu"
+        anchorEl={anchorEl}
+        open={!!anchorEl}
+        onClose={closeMenu}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem onClick={discordAuth}>Login via discord</MenuItem>
+      </Menu>
+    </React.Fragment>
+  );
+}
---
+++ src/pages/MemberLandingPage.jsx
@@ -0,0 +1,9 @@
+import { Stack, Typography } from "@mui/material";
+
+export default function MemberLandingPage() {
+  return (
+    <Stack>
+      <Typography>Logged in</Typography>
+    </Stack>
+  );
+}
