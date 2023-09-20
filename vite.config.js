a5269c4393b535b350897a7a5802af4464fd5470
Greg Ludington <Greg.Ludington@ipsoft.com>
2023-09-20 01:31:20 -0400

    update vite.config.js -- maybe netlify requires output in build dir?

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/',
   build: {
-    chunkSizeWarningLimit: 800
+    chunkSizeWarningLimit: 800,
+    outDir: './build'
   },
   plugins: [react()],
   server: {
