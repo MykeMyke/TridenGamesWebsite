68d696aceb5bc6994a063dc1a8545b41a98b3f7b
Greg Ludington <Greg.Ludington@ipsoft.com>
2023-09-20 00:30:19 -0400

    move from react-scripts to vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { ThemeProvider } from "@emotion/react";
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  server: {
+      port: 3000
+    },
+  test: {
+    environment: 'jsdom',
+  }
+})
