3b2a686d552daa05ca7df65fdae6d2129506daa0
OperationXen <OperationXen@apocryphasecurity.com>
2024-01-20 21:16:42 +0000

    Add playtest tickbox

--- src/api/utils.js
+++ src/api/utils.js
@@ -3,14 +3,19 @@ import Cookies from "js-cookie";
 /**
  * Get the appropriate host
  */
-export const apiHost = process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000" : "https://unseen-servant.tridengames.com";
- 
+export const apiHost =
+  process.env.NODE_ENV === "development2"
+    ? "http://127.0.0.1:8000"
+    : "https://unseen-servant.tridengames.com";
+
 /**
  * Apply the csrf headers
  * @param {*} headers an object containing existing headers
  * @returns an object hash with django csrftoken applied
  */
 export const applyCsrf = (headers) => {
-    const csrf = Cookies.get("csrftoken");
-    return headers ? { ...headers, "X-CSRFToken": csrf } : { "X-CSRFToken": csrf }
-}
+  const csrf = Cookies.get("csrftoken");
+  return headers
+    ? { ...headers, "X-CSRFToken": csrf }
+    : { "X-CSRFToken": csrf };
+};
