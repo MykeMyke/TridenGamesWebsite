e68c2ec2664dbeb43d9418b417769586441352e6
Greg Ludington <Greg.Ludington@ipsoft.com>
2024-01-20 17:56:22 -0500

    localize at least en and en-GB on date forms

--- src/components/game/DateTimeSelector.jsx
+++ src/components/game/DateTimeSelector.jsx
@@ -1,19 +1,32 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { DateTimePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { useFormikContext } from "formik";
-import moment from "moment";
+import moment from "moment/min/moment-with-locales";
 
 export default function DateTimeSelector(props) {
   const { values, errors, setFieldValue } = useFormikContext();
-
+  useEffect(() => {
+    
+  }, [navigator.language])
   const mValue = useMemo(() => {
-    return moment(values[props.name]);
-  }, [values[props.name]]);
+    if (values[props.name]) {
+      if (navigator?.language.indexOf('en-GB') > -1) {
+        moment.locale('en-GB');
+      } else {
+        moment.locale('en');
+      }
+      return moment(values[props.name]);
+    } else {
+      return undefined;
+    }
+  }, [values[props.name], navigator]);
+
+  console.info(moment.localeData().longDateFormat('L'))
   const error = errors?.[props.name];
   return (
-    <LocalizationProvider dateAdapter={AdapterMoment}>
+    <LocalizationProvider dateAdapter={AdapterMoment} dateFormats={"YYYY"}>
       <DateTimePicker
         label={props.label}
         value={mValue}
@@ -22,6 +35,7 @@ export default function DateTimeSelector(props) {
           setFieldValue(props.name, val.toDate());
           props?.onChange(val.toDate());
         }}
+        format={`${moment.localeData().longDateFormat('L')} HH:MM`}
         shouldDisableDate={(val) => {
           return !!error ? val.toDate().getTime() === mValue.toDate().getTime() : false;
         }}
