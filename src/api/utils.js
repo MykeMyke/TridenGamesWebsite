e99acbb51058b08e7e9f201ef047e61163eeb68f
OperationXen <OperationXen@apocryphasecurity.com>
2024-01-21 00:54:16 +0000

    Stop breaking the thing

--- src/api/utils.js
+++ src/api/utils.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
  * Get the appropriate host
  */
 export const apiHost =
-  process.env.NODE_ENV === "development2" ? "http://127.0.0.1:8000" : "https://unseen-servant.tridengames.com";
+  process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000" : "https://unseen-servant.tridengames.com";
 
 /**
  * Apply the csrf headers
