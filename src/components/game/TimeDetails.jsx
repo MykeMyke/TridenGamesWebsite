9e3f8826cded56cb80cc4aab855bafb29446afe1
OperationXen <OperationXen@apocryphasecurity.com>
2024-01-21 00:53:30 +0000

    Added staged release logic

--- src/components/game/TimeDetails.jsx
+++ src/components/game/TimeDetails.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useFormikContext } from "formik";
 
 import { Box, Button, IconButton, Tooltip, Checkbox } from "@mui/material";
 
@@ -8,8 +9,18 @@ import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import DateTimeSelector from "./DateTimeSelector";
 
 export default function TimeDetails(props) {
+  const { values, setFieldValue } = useFormikContext();
+
   const [showDetail, setShowDetail] = useState(false);
   const [stagedRelease, setStagedRelease] = useState(true);
+
+  const datetimeOpenRelease = values["datetime_open_release"];
+
+  useEffect(() => {
+    if (!stagedRelease) {
+      setFieldValue("datetime_release", datetimeOpenRelease);
+    }
+  }, [stagedRelease, datetimeOpenRelease]);
 
   return (
     <Box sx={{ display: "flex", flexDirection: "row", margin: "0.8em 0", justifyContent: "space-around" }}>
@@ -34,7 +45,7 @@ export default function TimeDetails(props) {
         <Checkbox disabled={!showDetail} checked={stagedRelease} sx={{ opacity: showDetail ? 1 : 0.2 }} />
       </Button>
       <DateTimeSelector
-        disabled={!showDetail}
+        disabled={!showDetail || !stagedRelease}
         sx={{ opacity: showDetail ? 1 : 0.2 }}
         label="Patreon Release"
         name="datetime_release"
