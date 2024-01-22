bcd25954f5beabe31d0385f2efc9d772c5791832
OperationXen <OperationXen@apocryphasecurity.com>
2024-01-22 19:43:20 +0000

    Datetime release can be null

--- src/components/game/GameCardActions.jsx
+++ src/components/game/GameCardActions.jsx
@@ -6,47 +6,58 @@ import { ReleaseDate } from "../../utils/releasedate";
 import useUserStore from "../../stores/useUserStore";
 import LoginButton from "../authentication/LoginButton";
 
+export default function GameCardActions({
+  id,
+  is_dm,
+  name,
+  datetime_release,
+  datetime_open_release,
+  playing,
+  standingBy,
+  joinGame,
+  isJoining,
+  dropGame,
+}) {
+  const user = useUserStore((s) => s.user);
+  const now = new Date();
+  if (!user?.loggedIn) {
+    return ReleaseDate(datetime_release, datetime_open_release);
+  }
 
-export default function GameCardActions({ id, is_dm, name, datetime_release, datetime_open_release, playing, standingBy, joinGame, isJoining, dropGame }) {
-    const user = useUserStore((s) => s.user);
-    const now = new Date();
-    if (!user?.loggedIn ) {
-        return ReleaseDate(datetime_release, datetime_open_release);
+  if (!is_dm) {
+    if (playing || standingBy) {
+      return (
+        <Button
+          aria-describedby={`drop-${id}`}
+          variant="contained"
+          disabled={isJoining}
+          onClick={() => dropGame({ id, name })}
+          size="small"
+          sx={{ pt: 0.25, pb: 0, mt: 0.4, mb: 1.1, mr: 1 }}
+          color="secondary"
+        >
+          Drop Now
+        </Button>
+      );
     }
-
-    if (!is_dm) {
-        if (playing || standingBy) {
-            return (
-                <Button
-                    aria-describedby={`drop-${id}`}
-                    variant="contained"
-                    disabled={isJoining}
-                    onClick={() => dropGame({ id, name })}
-                    size="small"
-                    sx={{ pt: 0.25, pb: 0, mt: 0.4, mb: 1.1, mr: 1 }}
-                    color="secondary"
-                >
-                    Drop Now
-                </Button>
-            )
-        }
-        if ((user.patreon && datetime_release.getTime() < now.getTime()) || datetime_open_release.getTime() < now.getTime()) {
-            return (
-                <Button
-                    aria-describedby={`join-${id}`}
-                    variant="contained"
-                    disabled={isJoining}
-                    onClick={() => joinGame({ id, name })}
-                    size="small"
-                    sx={{ pt: 0.25, pb: 0, mt: 0.4, mb: 1.1, mr: 1 }}
-                    color="secondary"
-                >
-                    Join Now
-                </Button>
-            )
-        }
+    if (
+      (user.patreon && datetime_release?.getTime() < now.getTime()) ||
+      datetime_open_release.getTime() < now.getTime()
+    ) {
+      return (
+        <Button
+          aria-describedby={`join-${id}`}
+          variant="contained"
+          disabled={isJoining}
+          onClick={() => joinGame({ id, name })}
+          size="small"
+          sx={{ pt: 0.25, pb: 0, mt: 0.4, mb: 1.1, mr: 1 }}
+          color="secondary"
+        >
+          Join Now
+        </Button>
+      );
     }
-    return (
-        ReleaseDate(datetime_release, datetime_open_release)
-    );
+  }
+  return ReleaseDate(datetime_release, datetime_open_release);
 }
