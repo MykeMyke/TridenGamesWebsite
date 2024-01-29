d5981569e35076a9ddac146e4345f36173211dd6
OperationXen <OperationXen@apocryphasecurity.com>
2024-01-29 21:03:27 +0000

    Integrated with API changes

--- src/api/dm_profile.js
+++ src/api/dm_profile.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+import { apiHost, applyCsrf } from "./utils";
+
 export function getUserDMProfile() {
-  return axios.get(`${apiHost}/dungeonmaster`, { withCredentials: true });
+  return axios.get(`${apiHost}/api/dungeonmasters/me`, { withCredentials: true });
+}
+
+export function updateUserDMProfile(updateData) {
+  return axios.patch(`${apiHost}/api/dungeonmasters/me/`, updateData, { withCredentials: true, headers: applyCsrf() });
 }
--- src/pages/authenticated/DMProfile.jsx
+++ src/pages/authenticated/DMProfile.jsx
@@ -1,15 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Container, Typography, Divider, Box } from "@mui/material";
 import { TextField, Button } from "@mui/material";
 
+import { capitalise } from "../../utils/formatting";
+
+import useAlertStore from "../../stores/useAlertStore";
+import useUserStore from "../../stores/useUserStore";
+
+import { getUserDMProfile, updateUserDMProfile } from "../../api/dm_profile";
+
 export default function DMProfile() {
+  const snackBar = useAlertStore((s) => s.setSuccess);
+  const user = useUserStore((s) => s.user);
+
   const [loading, setLoading] = useState(true);
+  const [refresh, setRefresh] = useState(true);
   const [profileExists, setProfileExists] = useState(false);
   const [dmName, setDMName] = useState("");
   const [dmDescription, setDMDescription] = useState("");
   const [dmRuleText, setDMRuleText] = useState("");
   const [dmMusterText, setDMMusterText] = useState("");
+
+  useEffect(() => {
+    if (!refresh) return;
+
+    getUserDMProfile()
+      .then((result) => {
+        setProfileExists(true);
+        setDMName(result.data.name);
+        setDMDescription(result.data.description);
+        setDMRuleText(result.data.rule_text);
+        setDMMusterText(result.data.muster_text);
+      })
+      .finally(() => {
+        setLoading(false);
+        setRefresh(false);
+      });
+  }, [refresh]);
+
+  const handleUpdate = () => {
+    updateUserDMProfile({
+      name: dmName !== "" ? dmName : `DM ${capitalise(user.username)}`,
+      description: dmDescription,
+      rules_text: dmRuleText,
+      muster_text: dmMusterText,
+    })
+      .then(() => {
+        snackBar("Updated DM profile");
+      })
+      .finally(() => {
+        setRefresh(true);
+      });
+  };
 
   return (
     <Container>
@@ -17,8 +60,15 @@ export default function DMProfile() {
         <Typography variant="h3">DM Profile Settings</Typography>
         <Divider />
 
-        <TextField value={dmName} onChange={(e) => setDMName(e.target.value)} placeholder="DM Delvemeister" label="DM Alias" />
         <TextField
+          disabled={loading}
+          value={dmName}
+          onChange={(e) => setDMName(e.target.value)}
+          placeholder="DM Delvemeister"
+          label="DM Alias"
+        />
+        <TextField
+          disabled={loading}
           value={dmDescription}
           onChange={(e) => setDMDescription(e.target.value)}
           placeholder="A brief description of yourself as a DM"
@@ -27,6 +77,7 @@ export default function DMProfile() {
           rows={3}
         />
         <TextField
+          disabled={loading}
           value={dmRuleText}
           onChange={(e) => setDMRuleText(e.target.value)}
           placeholder="Any rules you may wish to set for your games, you could for example demand all PC information is provided at least 24 hours before the game"
@@ -35,6 +86,7 @@ export default function DMProfile() {
           rows={4}
         />
         <TextField
+          disabled={loading}
           value={dmMusterText}
           onChange={(e) => setDMMusterText(e.target.value)}
           placeholder="Specify the text that Unseen Servant will post at the top of your mustering channels"
@@ -43,9 +95,25 @@ export default function DMProfile() {
           rows={4}
         />
 
-        <Button variant="outlined" sx={{ width: "60%", margin: "auto" }}>
+        <Button
+          variant={profileExists ? "outlined" : "contained"}
+          sx={{ width: "60%", margin: "auto" }}
+          onClick={handleUpdate}
+          disabled={loading}
+        >
           {profileExists ? "Update" : "Create"}
         </Button>
+        {!profileExists && (
+          <React.Fragment>
+            <Divider />
+            <Typography variant="caption" sx={{ margin: "auto" }}>
+              You have not yet created your DM profile, until you do you will be unable to create games
+            </Typography>
+            <Typography variant="caption" sx={{ margin: "auto" }}>
+              The above fields are optional, you can just click create and Unseen Servant will build you a basic profile
+            </Typography>
+          </React.Fragment>
+        )}
       </Box>
     </Container>
   );
